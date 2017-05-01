ac89193db080808b66b903e499a21bdf603c3f9d
Hiroyuki Suzuki <sainome.with.sopwith@gmail.com>
2017-05-01 11:25:27 +0900

    fix sqlite sample (but data display is not good)

--- moch-tests/bottle/sqlite.py
+++ moch-tests/bottle/sqlite.py
@@ -1,26 +1,31 @@
 from bottle import route, run, template
+import bottle.ext.sqlite
+import bottle
 import os
 
 # setup bottle - sqlite
 app = bottle.Bottle()
-plugin = bottle.ext.sqlite.Plugin(dbfile='./data/test.db')
+plugin = bottle.ext.sqlite.Plugin(dbfile='./test.db')
 app.install(plugin)
 
-@route('/hello/<name>')
+@app.route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-@route('/add/:id')
+@app.route('/add/:id')
 def add_id(id, db):
     # insert id into db
-    db.execute('INSERT INTO test (specified_id) VALUES (?)', id).fetchone()
+    db.execute('INSERT INTO test (specified_id) VALUES (?)', (id,))
+    db.commit()
     return template('{{id}} is added')
 
-@route('/show/:item')
+@app.route('/show/:item')
 def show_id(item, db):
-    row = db.execute('SELECT * FROM test WHERE specified_id=?', item).fetchone()
+    print("id -> {}".format(item))
+    row = db.execute('SELECT * FROM test WHERE specified_id=?', (item,)).fetchone()
     if row:
-        return template('showitem', page=row)
+        print(row)
+        return template('<p>data display : {{row}}</p>', row=row)
     return template('not found : {{item}}')
 
 SERVER_ADDRESS = '192.168.0.1'
@@ -30,5 +35,5 @@ if "SERVER_ADDRESS" in os.environ:
 
 print("Server -> {}:{}".format(SERVER_ADDRESS, SERVER_PORTNO))
 
-run(host=SERVER_ADDRESS, port=SERVER_PORTNO)
+app.run(host=SERVER_ADDRESS, port=SERVER_PORTNO, reloader=True)
 
